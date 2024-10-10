d3188fdc973b3273348ecf7a06462c666e1390e2
juliangithub04 <agudelo.julian@correounivalle.edu.co>
2024-10-10 01:03:05 -0500

    login y registro incompletos

---
+++ login/scriptlogin.js
@@ -0,0 +1,18 @@
+// Obtener los usuarios guardados en el localStorage
+let usuariosRegistrados = JSON.parse(localStorage.getItem('usuarios')) || {};
+
+// Función para manejar el evento de inicio de sesión
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Evitar que el formulario se envíe de manera predeterminada
+
+    // Obtener los valores del formulario
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    // Verificar si el usuario está registrado y la contraseña es correcta
+    if (usuariosRegistrados[email] && usuariosRegistrados[email].password === password) {
+        document.getElementById('login-mensaje').innerText = 'Inicio de sesión exitoso.';
+    } else {
+        document.getElementById('login-mensaje').innerText = 'Correo o contraseña incorrectos.';
+    }
+});
